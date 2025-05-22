ab5a5be253d6287568acbbf0176873bc3fc45b56
kingmaker216 <gorakhnathgodse@gmail.com>
2025-05-23 00:41:49 +0530

    Add files via upload

---
+++ Assignment 3 factorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+ans =factorial(5)
+print(ans)
+
---
+++ Assignment 3.py
@@ -0,0 +1,17 @@
+import math
+
+def calculate_math_functions():
+            num_str = input("Enter a number: ")
+            number = float(num_str)
+            square_root = math.sqrt(number)
+            print("Square Root:", square_root)
+            natural_logarithm = math.log(number)
+            print("Logarithm: ", natural_logarithm)
+            sine_value = math.sin(number)
+            print("Sine : ", sine_value)
+
+calculate_math_functions()
+
+
+
+
